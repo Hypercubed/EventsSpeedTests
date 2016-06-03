110d3eacb1675a600afaff959fd55d6f34c75323
Jayson Harshbarger <hypercubed@gmail.com>
2016-06-03 10:00:35 +0900

    Update grunion

--- package-scripts.js
+++ package-scripts.js
@@ -15,12 +15,13 @@ module.exports = {
   scripts: {
     build: `grunion --run "${browserify}" ${nodeScripts}`,
     bench: {
-      node: `grunion ${nodeScripts} --serial`,
-      node4: `grunion ${nodeScripts} --serial | tee ./raw/node-v4.4.md`,
-      node6: `grunion ${nodeScripts} --serial | tee ./raw/node-v6.2.md`,
-      chrome: `grunion --run "testling -x \\"${browsers.chrome}\\" < <%= file.path %>" ${browserifiedScripts} --serial | tee ./raw/browser-chrome-v50.md`,
-      firefox: `grunion --run "testling -x \\"${browsers.firefox}\\" < <%= file.path %>" ${browserifiedScripts} --serial | tee ./raw/browser-firefox-v46.md`,
-      safari: `grunion --run "testling -x \\"${browsers.safari}\\" < <%= file.path %>" ${browserifiedScripts} --serial | tee ./raw/browser-safari-v9.md`
+      node: `grunion ${nodeScripts} --serial --raw`,
+      node4: `grunion ${nodeScripts} --serial --raw | tee ./raw/node-v4.4.md`,
+      node6: `grunion ${nodeScripts} --serial --no-raw | tee ./raw/node-v6.2.md`,
+      browser: `grunion --run "testling -x \\"${browsers.chrome}\\" < <%= file.path %>" ${browserifiedScripts} --serial --raw`,
+      chrome: `grunion --run "testling -x \\"${browsers.chrome}\\" < <%= file.path %>" ${browserifiedScripts} --serial --raw | tee ./raw/browser-chrome-v50.md`,
+      firefox: `grunion --run "testling -x \\"${browsers.firefox}\\" < <%= file.path %>" ${browserifiedScripts} --serial --raw | tee ./raw/browser-firefox-v46.md`,
+      safari: `grunion --run "testling -x \\"${browsers.safari}\\" < <%= file.path %>" ${browserifiedScripts} --serial --raw | tee ./raw/browser-safari-v9.md`
     },
     summary: {
       default: `grunion --run "${tapMarkdown} < <%= file.path %> > ./results/<%= file.base %>" ./raw/*.md`,
